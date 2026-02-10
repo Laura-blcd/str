d8e60715d3e643cc10e72706084dd9c8a83bb0e4
Laura Blanchard <blanchardlaura5@gmail.com>
2026-02-10 21:56:56 +0100

    controller

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -1,7 +1,24 @@
-<html>
-    <body>
-        <h1>singe</h1>
-        <h1> 67 </h1>
-        <p> grand singe </p>
-    </body>
-</html>
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/', name: 'str_homepage', methods: ['GET'])]
+    public function index(): Response
+    {
+        /* return $this->json([
+            'message' => 'Welcome to the homepage!',
+            'status' => 'success',
+        ]);*/
+
+        return $this->render('homepage.html.twig');
+    }
+}
